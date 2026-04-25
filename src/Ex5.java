93de02ee3ac2a65075a4367ab65a5b0384fbac7d
Florea Alina <floreaalina340@gmail.com>
2026-04-25 13:21:46 +0300

    Add Ex5

---
+++ src/Ex5.java
@@ -0,0 +1,13 @@
+public class Ex5 {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7};
+        int sum = 0;
+        for ( int number : arr) {
+             sum = sum + number;
+        }
+
+        System.out.println(sum);
+
+    }
+}
