4beb3dd38f320a430de8e213c92572161c427d31
Florea Alina <floreaalina340@gmail.com>
2026-04-25 14:35:49 +0300

    Add Ex 10

---
+++ src/Ex10.java
@@ -0,0 +1,11 @@
+public class Ex10 {
+    public static void main(String[] args) {
+
+        for(int  i = 0; i <= 32; i++) {
+            if (i % 2 == 0 ) continue;
+
+            System.out.println(i);
+        }
+
+    }
+}
