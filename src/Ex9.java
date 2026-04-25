9d36c5fb9394125817cd96410f0fdf8901ab85dc
Florea Alina <floreaalina340@gmail.com>
2026-04-25 14:35:12 +0300

    Add EX9

---
+++ src/Ex9.java
@@ -0,0 +1,12 @@
+public class Ex9 {
+
+
+    static int Perimetru(int latura) {
+        return  4 * latura;
+    }
+
+    public static void main(String[] args) {
+        int rezultat = Perimetru(8);
+        System.out.println(rezultat);
+    }
+}
