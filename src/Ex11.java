305b82fed1f23d0f3e177b237648b134f214f0d8
Florea Alina <floreaalina340@gmail.com>
2026-04-25 14:36:17 +0300

    Add Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,16 @@
+public class Ex11 {
+    public static void main(String[] args) {
+
+        int number = 10;
+        do {
+            System.out.println(number);
+        } while(number < 7 );
+
+
+    }
+}
+
+
+//       mijloc transport
+//        avion
+//        masina
