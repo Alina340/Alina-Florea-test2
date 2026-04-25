404f0d32af56c8d5f1de05d1d5cd8405c7bf07e5
Florea Alina <floreaalina340@gmail.com>
2026-04-25 13:09:49 +0300

    Add Ex3

---
+++ src/Ex3.java
@@ -0,0 +1,11 @@
+public class Ex3 {
+    public static void main(String[] args) {
+
+        int number = 20;
+
+        while(number <= 40) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
